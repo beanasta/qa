2bc978309b97afa5b06e5a800e0c5e3206f6b724
Anastasia <Beanasta@mail.ru>
2022-09-04 22:49:20 +0300

    training

--- src/main.java
+++ src/main.java
@@ -1,11 +1,11 @@
 public class main {
     public static void main(String[] args) {
-        int eaters = 5; // сколько людей будут есть
+        int eaters = 2; // сколько людей будут есть
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        int water = 500; // миллилитров воды
+        int potatoes =2; // картофелин
+        int chicken = 2; // куриных бёдер
+        int spices = 2; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
