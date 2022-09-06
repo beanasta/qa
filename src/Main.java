e0bca6fe7a5cbce0f365f5c9a6491965625b92d7
Anastasia <Beanasta@mail.ru>
2022-09-06 10:04:02 +0300

    Name change

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
+
         int eaters = 5; // сколько людей будут есть
 
         int water = 3000; // миллилитров воды
-        int potatoes =5; // картофелин
+        int potatoes = 5; // картофелин
         int chicken = 6; // куриных бёдер
         int spices = 10; // ложек специй
 
@@ -12,5 +13,6 @@ public class main {
         System.out.println((potatoes / eaters) + " картофелин(а)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
+
     }
 }
